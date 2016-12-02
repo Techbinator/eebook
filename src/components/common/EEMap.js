de2786cd923c70a4cdcdc6222ed85b95b60fe1fe
tudor filipovici <tudor@tudor.filipovici>
2016-12-02 17:39:43 +0100

    Add real data to map

--- src/components/common/EEMap.js
+++ src/components/common/EEMap.js
@@ -22,7 +22,8 @@ export default class EEMap extends Component {
     var dataProvider = {
       mapVar: AmCharts.maps.worldLow,
       linkToObject: this.props.linkToObject,
-      images: this.props.cities
+      images: this.props.cities,
+      zoomLevel : this.props.zoomLevel,
     };
     this.map.dataProvider  = dataProvider;
   }
@@ -56,7 +57,9 @@ EEMap.defaultProps = {
     unlistedAreasColor: '#CCCCCC'
   },
   cities: [],
-  linkToObject: {}
+  linkToObject: null,
+  zoomLevel: 7
+
 }
 
 export const targetSVG = "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z";
--- src/components/pages/search/sub/SearchMap.js
+++ src/components/pages/search/sub/SearchMap.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import EEMap, {planeSVG, targetSVG} from '../../../common/EEMap'
+import { connect } from 'react-redux'
+import { map, uniqBy, filter } from 'lodash'
 
 // LONDON
 var london = {
@@ -73,174 +75,53 @@ var london = {
 };
 
 
-// VILNIUS
-var vilnius = {
-    id: "vilnius",
-    color: "#000000",
-    svgPath: targetSVG,
-    title: "Vilnius",
-    latitude: 54.6896,
-    longitude: 25.2799,
-    scale: 1.5,
-    zoomLevel: 4.92,
-    zoomLongitude: 15.4492,
-    zoomLatitude: 50.2631,
+class SearchMap extends Component {
 
-    lines: [{
-        latitudes: [54.6896, 50.8371],
-        longitudes: [25.2799, 4.3676]},
-    {
-        latitudes: [54.6896, 59.9138],
-        longitudes: [25.2799, 10.7387]},
-    {
-        latitudes: [54.6896, 40.4167],
-        longitudes: [25.2799, -3.7033]},
-    {
-        latitudes: [54.6896, 50.0878],
-        longitudes: [25.2799, 14.4205]},
-    {
-        latitudes: [54.6896, 48.2116],
-        longitudes: [25.2799, 17.1547]},
-    {
-        latitudes: [54.6896, 44.8048],
-        longitudes: [25.2799, 20.4781]},
-    {
-        latitudes: [54.6896, 55.7558],
-        longitudes: [25.2799, 37.6176]},
-    {
-        latitudes: [54.6896, 37.9792],
-        longitudes: [25.2799, 23.7166]},
-    {
-        latitudes: [54.6896, 54.6896],
-        longitudes: [25.2799, 25.2799]},
-    {
-        latitudes: [54.6896, 51.5002],
-        longitudes: [25.2799, -0.1262]},
-    {
-        latitudes: [54.6896, 53.3441],
-        longitudes: [25.2799, -6.2675]}],
+  processData(){
+    if( this.props.originCode){
 
-    images: [{
-        label: "Flights from Vilnius",
-        svgPath: planeSVG,
-        left: 100,
-        top: 45,
-        color: "#CC0000",
-        labelColor: "#CC0000",
-        labelRollOverColor: "#CC0000",
-        labelFontSize: 20},
-    {
-        label: "show flights from London",
-        left: 110,
-        top: 70,
-        labelColor: "#000000",
-        labelRollOverColor: "#CC0000",
-        labelFontSize: 11,
-        linkToObject: "london"}]
-};
+    }
+  }
 
-// cities
-var cities = [
-    london,
-    vilnius,
-{
-    svgPath: targetSVG,
-    title: "Brussels",
-    latitude: 50.8371,
-    longitude: 4.3676},
-{
-    svgPath: targetSVG,
-    title: "Prague",
-    latitude: 50.0878,
-    longitude: 14.4205},
-{
-    svgPath: targetSVG,
-    title: "Athens",
-    latitude: 37.9792,
-    longitude: 23.7166},
-{
-    svgPath: targetSVG,
-    title: "Reykjavik",
-    latitude: 64.1353,
-    longitude: -21.8952},
-{
-    svgPath: targetSVG,
-    title: "Dublin",
-    latitude: 53.3441,
-    longitude: -6.2675},
-{
-    svgPath: targetSVG,
-    title: "Oslo",
-    latitude: 59.9138,
-    longitude: 10.7387},
-{
-    svgPath: targetSVG,
-    title: "Lisbon",
-    latitude: 38.7072,
-    longitude: -9.1355},
-{
-    svgPath: targetSVG,
-    title: "Moscow",
-    latitude: 55.7558,
-    longitude: 37.6176},
-{
-    svgPath: targetSVG,
-    title: "Belgrade",
-    latitude: 44.8048,
-    longitude: 20.4781},
-{
-    svgPath: targetSVG,
-    title: "Bratislava",
-    latitude: 48.2116,
-    longitude: 17.1547},
-{
-    svgPath: targetSVG,
-    title: "Ljubljana",
-    latitude: 46.0514,
-    longitude: 14.5060},
-{
-    svgPath: targetSVG,
-    title: "Madrid",
-    latitude: 40.4167,
-    longitude: -3.7033},
-{
-    svgPath: targetSVG,
-    title: "Stockholm",
-    latitude: 59.3328,
-    longitude: 18.0645},
-{
-    svgPath: targetSVG,
-    title: "Bern",
-    latitude: 46.9480,
-    longitude: 7.4481},
-{
-    svgPath: targetSVG,
-    title: "Kiev",
-    latitude: 50.4422,
-    longitude: 30.5367},
-{
-    svgPath: targetSVG,
-    title: "Paris",
-    latitude: 48.8567,
-    longitude: 2.3510},
-{
-    svgPath: targetSVG,
-    title: "New York",
-    latitude: 40.43,
-    longitude: -74}];
+  getOrigins() {
+    let options = this.props.options;
 
+    if (this.props.destinCode !== "") {
+      options = filter(options, {destinationCode: this.props.destinCode});
+    }
 
-export default class SearchMap extends Component {
+    let origins = map(options, (itm) => {
+      return {
+        title: itm.originName,
+        id: itm.originCode,
+        svgPath: targetSVG,
+        latitude: itm.originLat,
+        longitude: itm.originLon
+      };
+    });
 
-  // getData(){
-  //
-  // }
+    this.cities = uniqBy(origins, 'id');
+  }
 
   render(){
-
+      this.cities = false;
+    this.getOrigins();
     // this.getData();
-    console.log('cities', cities)
-    console.log('vilnius', vilnius)
-    return( <EEMap cities={cities}  linkToObject={vilnius} />)
+    var test = (this.props.originCode == 'FRA') ? vilnius : london;
+
+    if(this.cities){
+        return( <EEMap cities={this.cities}  />)
+    }
+
   }
 }
+
+function getProperties(state) {
+    return {
+        originCode: state.Search.originCode,
+        destinCode: state.Search.destinCode,
+        options:    state.Data.routes,
+    }
+}
+
+export default connect(getProperties)(SearchMap);
