81ee0c7c3d8b74bc59a3a1440c954f530e6661b7
tudor filipovici <tudor@tudor.filipovici>
2016-12-05 17:42:48 +0100

    Add flights for one way, return , selected origin and selected destination animation, tbd on click events and let side text

--- src/components/pages/search/sub/SearchMap.js
+++ src/components/pages/search/sub/SearchMap.js
@@ -1,116 +1,160 @@
 import React, {Component} from 'react'
-import EEMap, {planeSVG, targetSVG} from '../../../common/EEMap'
+import EEMap, {planeSVGFR, targetSVG, planeSVGLines, selectedExtra} from '../../../common/EEMap'
 import { connect } from 'react-redux'
-import { map, uniqBy, filter } from 'lodash'
+import { map, uniqBy, filter, find } from 'lodash'
 
-// LONDON
-var london = {
-    id: "london",
-    color: "#000000",
-    svgPath: targetSVG,
-    title: "London",
-    latitude: 51.5002,
-    longitude: -0.1262,
-    scale: 1.5,
-    zoomLevel: 2.74,
-    zoomLongitude: -20.1341,
-    zoomLatitude: 49.1712,
 
-    lines: [
-        {
-        latitudes: [51.5002, 50.4422],
-        longitudes: [-0.1262, 30.5367]},
-    {
-        latitudes: [51.5002, 46.9480],
-        longitudes: [-0.1262, 7.4481]},
-    {
-        latitudes: [51.5002, 59.3328],
-        longitudes: [-0.1262, 18.0645]},
-    {
-        latitudes: [51.5002, 40.4167],
-        longitudes: [-0.1262, -3.7033]},
-    {
-        latitudes: [51.5002, 46.0514],
-        longitudes: [-0.1262, 14.5060]},
-    {
-        latitudes: [51.5002, 48.2116],
-        longitudes: [-0.1262, 17.1547]},
-    {
-        latitudes: [51.5002, 44.8048],
-        longitudes: [-0.1262, 20.4781]},
-    {
-        latitudes: [51.5002, 55.7558],
-        longitudes: [-0.1262, 37.6176]},
-    {
-        latitudes: [51.5002, 38.7072],
-        longitudes: [-0.1262, -9.1355]},
-    {
-        latitudes: [51.5002, 54.6896],
-        longitudes: [-0.1262, 25.2799]},
-    {
-        latitudes: [51.5002, 64.1353],
-        longitudes: [-0.1262, -21.8952]},
-    {
-        latitudes: [51.5002, 40.4300],
-        longitudes: [-0.1262, -74.0000]}
-    ],
 
-    images: [{
-        label: "Flights from London",
-        svgPath: planeSVG,
+class SearchMap extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.clear()
+
+  }
+
+  clear(){
+    this.cities = []
+    this.lines = []
+    this.linkToObject = null
+  }
+
+  processData(){
+    this.clear()
+
+    //in case we have both origin and destination selected
+    if( this.props.originCode && this.props.destinCode){
+      this.setOriginAndDestination()
+    } else {
+      this.getOriginsOrDestin()
+      if(this.props.originCode || this.props.destinCode){
+        this.setSelectedCityData()
+      }
+    }
+  }
+
+  setOriginAndDestination(){
+
+    let origin = this.getSingleCityData('origin')
+
+    let destin = this.getSingleCityData('destin')
+
+    this.cities.push(origin)
+    this.cities.push(destin)
+    this.cities.push({
+        svgPath: planeSVGLines,
+        positionOnLine: 0,
+        color: "#585869",
+        animateAlongLine: true,
+        lineId: "line1",
+        flipDirection: !this.props.oneWay,
+        loop: true,
+        scale: 0.03,
+        positionScale: 1.8
+    })
+
+    this.lines.push({
+      id: "line1",
+      arc: -0.85,
+      alpha: 0.3,
+      latitudes: [origin.latitude, destin.latitude],
+      longitudes: [origin.longitude, destin.longitude]
+    })
+  }
+
+  getSingleCityData( type = 'origin'){
+
+    let cityData = [];
+
+    if( type == 'origin'){
+      cityData =  _.find(this.props.options, (x) => { return x.originCode == this.props.originCode })
+      return {
+        id: cityData.originCode,
+        title: cityData.originName,
+        latitude: cityData.originLat,
+        longitude: cityData.originLon,
+        svgPath: targetSVG,
+        ...selectedExtra
+      }
+    } else {
+      cityData =  _.find(this.props.options, (x) => { return x.destinationCode == this.props.destinCode })
+      return {
+        id: cityData.destinationCode,
+        title: cityData.destinationName,
+        latitude: cityData.destinLat,
+        longitude: cityData.destinLon,
+        svgPath: targetSVG,
+        ...selectedExtra
+      }
+    }
+
+  }
+
+  getOriginsOrDestin() {
+    let cities = this.props.options;
+
+    if (this.props.destinCode !== "") {
+      cities = filter(cities, {destinationCode: this.props.destinCode});
+    }else if (this.props.originCode !== "") {
+      cities = filter(cities, {originCode: this.props.originCode});
+    }
+
+     cities = map(cities, (itm) => {
+      if( this.props.destinCode || ( !this.props.destinCode && !this.props.originCode)){
+        return {
+          title: itm.originName,
+          id: itm.originCode,
+          svgPath: targetSVG,
+          latitude: itm.originLat,
+          longitude: itm.originLon
+        };
+      } else {
+        return {
+          title: itm.destinationName,
+          id: itm.destinationCode,
+          svgPath: targetSVG,
+          latitude: itm.destinLat,
+          longitude: itm.destinLon
+        };
+      }
+
+    });
+
+    this.cities = uniqBy(cities, 'id');
+  }
+
+  setSelectedCityData(){
+    let selectedCity
+    if( this.props.destinCode){
+      selectedCity = this.getSingleCityData( 'destin')
+    } else {
+      selectedCity = this.getSingleCityData( 'origin')
+    }
+    selectedCity.lines = map(this.cities, (city) => {
+      return {
+        latitudes: [selectedCity.latitude, city.latitude],
+        longitudes: [selectedCity.longitude, city.longitude]
+      }
+    })
+    selectedCity.images = [{
+        label: `Flights from ${selectedCity.title}`,
+        svgPath: planeSVGFR,
         left: 100,
         top: 45,
         color: "#CC0000",
         labelColor: "#CC0000",
         labelRollOverColor: "#CC0000",
-        labelFontSize: 20},
-    {
-        label: "show flights from Vilnius",
-        left: 110,
-        top: 70,
-        labelColor: "#000000",
-        labelRollOverColor: "#CC0000",
-        labelFontSize: 11,
-        linkToObject: "vilnius"}]
-};
-
-
-class SearchMap extends Component {
-
-  processData(){
-    if( this.props.originCode){
-
-    }
-  }
-
-  getOrigins() {
-    let options = this.props.options;
-
-    if (this.props.destinCode !== "") {
-      options = filter(options, {destinationCode: this.props.destinCode});
-    }
-
-    let origins = map(options, (itm) => {
-      return {
-        title: itm.originName,
-        id: itm.originCode,
-        svgPath: targetSVG,
-        latitude: itm.originLat,
-        longitude: itm.originLon
-      };
-    });
-
-    this.cities = uniqBy(origins, 'id');
+        labelFontSize: 20
+    }]
+    this.cities.push(selectedCity);
+    this.linkToObject = selectedCity;
   }
 
   render(){
-      this.cities = false;
-    this.getOrigins();
-    // this.getData();
-    var test = (this.props.originCode == 'FRA') ? vilnius : london;
-
+    this.processData()
     if(this.cities){
-        return( <EEMap cities={this.cities}  />)
+        return( <EEMap cities={this.cities} lines={this.lines} linkToObject={this.linkToObject} />)
     }
 
   }
@@ -120,6 +164,7 @@ function getProperties(state) {
     return {
         originCode: state.Search.originCode,
         destinCode: state.Search.destinCode,
+        oneWay: (state.Search.numJourneys == 2) ? false : true,
         options:    state.Data.routes,
     }
 }
--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,43 +1,4 @@
-import { map, uniqBy, filter } from 'lodash'
 
 export function escapeRegExp(str) {
   return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 }
-
-export function getOrigins(props) {
-
-   let options = props.options;
-
-   if (props.destinCode !== "") {
-     options = filter(options, {destinationCode: props.destinCode});
-   }
-
-   let origins = map(options, (itm) => {
-     return {
-       label: itm.originName,
-       value: itm.originCode
-     };
-   });
-
-   origins = uniqBy(origins, 'value');
-   return origins;
- }
-
- export function getDestinations(props, extra = [], origin) {
-
-   let options = props.options;
-
-   if (props.originCode !== "") {
-     options = filter(options, {originCode: props.originCode});
-   }
-
-   let destins = map(options, itm => {
-     return {
-       label: itm.destinationName,
-       value: itm.destinationCode
-     }
-   });
-
-   destins = uniqBy(destins, 'value');
-   return destins;
- }
